1319180a6a3db56a88fdc3d0a9f7c022b1bea52d
sungheena <na.sunghee1@gmail.com>
2022-10-02 16:08:09 +0900

    feat:
    hello test코드 확인

---
+++ src/main/java/com/springbookaws/book/doremi/web/dto/HelloResponseDto.java
@@ -0,0 +1,12 @@
+package com.springbookaws.book.doremi.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+}
